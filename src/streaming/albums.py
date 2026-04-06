7b20884aa9365d4c5f6e2848d28dc99c3f6a9e39
dager1808 <25IMC30123@imc.ac.at>
2026-04-06 18:28:03 +0200

    Update albums.py
    changes in album

--- src/streaming/albums.py
+++ src/streaming/albums.py
@@ -6,3 +6,21 @@ Implement the Album class for collections of AlbumTrack objects.
 Classes to implement:
   - Album
 """
+class Album:
+    def __init__(self, album_id: str, title: str, artist, release_year: int) -> None:
+        self.album_id = album_id
+        self.title = title
+        self.artist = artist
+        self.release_year = release_year
+        self.tracks = []
+
+    def add_track(self, track) -> None:
+        track.album = self
+        self.tracks.append(track)
+        self.tracks.sort(key=lambda t: t.track_number)
+
+    def track_ids(self) -> set[str]:
+        return {track.track_id for track in self.tracks}
+
+    def duration_seconds(self) -> int:
+        return sum(track.duration_seconds for track in self.tracks)
